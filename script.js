3a9f19326306fa8d5ed5d39f833e444d4fd3149a
Aidas Baublys <aidas.baublys.darbo@gmail.com>
2022-02-09 13:40:58 +0200

    Add cool animation logic

--- script.js
+++ script.js
@@ -1,8 +1,105 @@
+let canvas;
+let ctx;
+let flowField;
+let flowFieldAnimation;
+
 window.onload = function () {
-  const canvas = document.getElementById("canvas-1");
-  const ctx = canvas.getContext("2d");
+  canvas = document.getElementById("canvas-1");
+
+  ctx = canvas.getContext("2d");
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
+
+  flowField = new FlowFieldEffect(ctx, canvas.width, canvas.height);
+  flowField.animate(0);
 };
 
-class FlowFieldEffect {}
+window.addEventListener("resize", function () {
+  cancelAnimationFrame(flowFieldAnimation);
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+  flowField = new FlowFieldEffect(ctx, canvas.width, canvas.height);
+  flowField.animate(0);
+});
+
+const mouse = {
+  x: 0,
+  y: 0,
+};
+
+window.addEventListener("mousemove", function (e) {
+  mouse.x = e.x;
+  mouse.y = e.y;
+});
+
+class FlowFieldEffect {
+  /** @type {CanvasRenderingContext2D} */
+  #ctx;
+  #width;
+  #height;
+
+  constructor(ctx, width, height) {
+    this.#ctx = ctx;
+    this.#width = width;
+    this.#height = height;
+
+    this.#ctx.lineWidth = 1;
+
+    this.lastTime = 0;
+    this.interval = 1000 / 60;
+    this.timer = 0;
+
+    this.cellSize = 15;
+    this.gradient;
+    this.#createGradient();
+    this.#ctx.strokeStyle = this.gradient;
+    this.radius = 0;
+    this.vr = 0.03;
+  }
+
+  #createGradient() {
+    this.gradient = this.#ctx.createLinearGradient(
+      0,
+      0,
+      this.#width,
+      this.#height,
+    );
+
+    this.gradient.addColorStop("0.1", "#ff5c33");
+    this.gradient.addColorStop("0.2", "#ff66b3");
+    this.gradient.addColorStop("0.4", "#ccccff");
+    this.gradient.addColorStop("0.6", "#b3ffff");
+    this.gradient.addColorStop("0.8", "#80ff80");
+    this.gradient.addColorStop("0.9", "#ffff33  ");
+  }
+
+  #drawLine(angle, x, y) {
+    this.#ctx.beginPath();
+    this.#ctx.moveTo(x, y);
+    this.#ctx.lineTo(x + Math.cos(angle) * 30, y + Math.sin(angle) * 30);
+    this.#ctx.stroke();
+  }
+
+  animate(timestamp) {
+    const deltaTime = timestamp - this.lastTime;
+    this.lastTime = timestamp;
+
+    if (this.timer > this.interval) {
+      this.#ctx.clearRect(0, 0, this.#width, this.#height);
+      this.radius += this.vr;
+
+      for (let y = 0; y < this.#height; y += this.cellSize) {
+        for (let x = 0; x < this.#width; x += this.cellSize) {
+          const angle = (Math.cos(x * 0.01) + Math.sin(y * 0.01)) * this.radius;
+          this.#drawLine(angle, x, y);
+        }
+      }
+
+      this.timer = 0;
+    } else {
+      this.timer += deltaTime;
+    }
+
+    flowFieldAnimation = requestAnimationFrame(this.animate.bind(this));
+  }
+}
